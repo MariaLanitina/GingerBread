ec9bb7a6021dd7185cfa7f227cdd46fe73fbe04c
Maria Lanitina <greenboba.gut@yandex.ru>
2018-06-03 15:12:30 +0300

    Task3

---
+++ task3.rb
@@ -0,0 +1,18 @@
+t1 = ARGV[0]
+t2 = ARGV[1]
+if t1 >= 1 && t2 <= 100000
+  summ = t1 + t2
+  whole = (summ / 60).floor
+  deduction = summ - whole * 60
+  if (summ < 3600)
+    puts "0 hours  #{whole} minutes #{deduction} seconds"
+  elsif summ >= 3600 
+    whole = (whole / 60).floor
+    residue = summ - (whole * 3600)
+    min = (residue / 60).floor
+    sec = residue - min * 60 
+    puts "#{whole} hours #{min} minutes #{sec}  seconds"
+  end
+else
+  puts "Error"
+end
