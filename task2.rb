8e5106b637f48ebfc2f33f3f4cef9ef86cf629da
Maria Lanitina <39571100+MariaLanitina@users.noreply.github.com>
2018-06-06 16:11:17 +0300

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,21 +1,13 @@
 day = ARGV[0].to_i
-
 month = ARGV[1].to_i
-
 year = ARGV[2].to_i
-
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
 if  year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
   days_in_month[1] = 29
 end
-
 num_of_days = []
 num_of_days = days_in_month.take(month - 1) if month > 1
-
 days = 0
 num_of_days.each { |v| days += v }
-
 days_from_start = days + day
-
 puts 356 - days_from_start
