389297bec23cf7b1036892447c7863449276985d
Maria Lanitina <39571100+MariaLanitina@users.noreply.github.com>
2018-06-10 16:46:01 +0300

    Update task1.rb

--- task1.rb
+++ task1.rb
@@ -3,7 +3,7 @@ current_system = ARGV[1].to_i
 result_system = ARGV[2].to_i
 if (value.to_i >= 0 && value.to_i <= 10000) && (current_system >= 2) && (result_system <= 36)
   value = value.to_i(current_system)
-  result_value = value.to_s(result)
+  result_value = value.to_s(result_system)
   puts result_value.to_i
 else
   puts "Wrong Data"
