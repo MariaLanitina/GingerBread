73a479a81d97a2b05a43f07b7ec3c267bc8baacd
Maria Lanitina <39571100+MariaLanitina@users.noreply.github.com>
2018-06-11 14:40:05 +0300

    Update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,18 +1,14 @@
 t1 = ARGV[0].to_i
 t2 = ARGV[1].to_i
-if t1 >= 1 && t2 <= 100000
-  summ = t1 + t2
-  whole = (summ / 60).floor
-  deduction = summ - whole * 60
-    if summ < 3600
-      puts "0 hours  #{whole} minutes #{deduction} seconds"
-    elsif summ >= 3600 
-      whole = (whole / 60).floor
-      residue = summ - (whole * 3600)
-      min = (residue / 60).floor
-      sec = residue - min * 60 
-      puts "#{whole} hours #{min} minutes #{sec}  seconds"
-    end
-else
-  puts "Error"
-end
+summ = t1 + t2
+whole = (summ / 60).floor
+deduction = summ - whole * 60
+  if summ < 3600
+    puts "0 hours  #{whole} minutes #{deduction} seconds"
+  elsif summ >= 3600 
+    whole = (whole / 60).floor
+    residue = summ - whole * 3600
+    min = (residue / 60).floor
+    sec = residue - min * 60 
+    puts "#{whole} hours #{min} minutes #{sec}  seconds"
+  end
