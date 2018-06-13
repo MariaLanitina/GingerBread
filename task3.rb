8a799e771915d76419e1a46700fd02c4e7b8ab73
Maria Lanitina <39571100+MariaLanitina@users.noreply.github.com>
2018-06-13 16:21:55 +0300

    Update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,14 +1,4 @@
 t1 = ARGV[0].to_i
 t2 = ARGV[1].to_i
-summ = t1 + t2
-whole = (summ / 60).floor
-deduction = summ - whole * 60
-  if summ < 3600
-    puts "0 hours  #{whole} minutes #{deduction} seconds"
-  elsif summ >= 3600 
-    whole = (whole / 60).floor
-    residue = summ - whole * 3600
-    min = (residue / 60).floor
-    sec = residue - min * 60 
-    puts "#{whole} hours #{min} minutes #{sec}  seconds"
-  end
+t = t1 + t2
+p Time.at(t).utc.strftime("%H hours %M minutes %S second")
