a4bc0e83522e28e3444265e6617c93c6bfc3e670
Maria Lanitina <39571100+MariaLanitina@users.noreply.github.com>
2018-06-16 14:25:10 +0300

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,4 +1,9 @@
-day = ARGV[0].to_i 
-month = ARGV[1].to_i 
-year = ARGV[2].to_i 
-puts (Time.new(year, 12, 31) - Time.new(year, month, day)).to_i / 86400
+day = ARGV[0].to_i
+month = ARGV[1].to_i
+year = ARGV[2].to_i
+days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = []
+num_of_days = days_in_month.take(month - 1) if month > 1
+days = 0
+num_of_days.each { |v| days += v }
+puts 365 - days - day
